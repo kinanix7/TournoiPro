b82e6933f7af27ef2015e7507c4652eb6203ebdf
Youness elkinani <kinanix7>
2025-09-07 14:49:04 +0100

    first commit

---
+++ src/main/java/com/tournoipro/TournoiProApplication.java
@@ -0,0 +1,13 @@
+package com.tournoipro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournoiProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TournoiProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tournoipro/TournoiProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tournoipro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournoiProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
